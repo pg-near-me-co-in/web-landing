1f9f13537fe77beb8ca5dbf2c2f9e406016e00c4
Dip Vachhani <dipv@digiflux.io>
2026-08-26 22:20:25 +0530

    Remove all

--- e2e/smoke.spec.ts
+++
@@ -1,6 +0,0 @@
-import { test, expect } from "@playwright/test";
-
-test("home responds 200", async ({ page }) => {
-  const response = await page.goto("/");
-  expect(response?.status()).toBe(200);
-});
